f46d175343963606886c39d948b73c93a24631c2
geenva <geenva@users.noreply.github.com>
2021-02-22 21:04:43 +0800

    feat: dontasktoask

---
+++ src/commands/general/dontasktoask.ts
@@ -0,0 +1,12 @@
+import { Command as CMD } from "../..";
+
+const Command: CMD = {
+  info: {
+    name: "dontasktoask",
+    title: "Don't ask to ask, just ask.",
+    res: "[Here's why](https://dontasktoask.com).",
+    aliases: ["data"],
+  },
+};
+
+export default Command;
