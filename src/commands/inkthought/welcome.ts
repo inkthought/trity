535012396508efb40f01d2bfb8a2ee40a38958da
geenva <geenva@users.noreply.github.com>
2021-02-22 21:02:37 +0800

    feat: welcome

--- src/commands/inkthought/welcome.ts
+++ src/commands/inkthought/welcome.ts
@@ -4,7 +4,8 @@ const Command: CMD = {
   info: {
     name: "inkthought",
     title: "Welcome to *inkthought labs!*",
-    res: "Here, we do lots of stuff. From programming, to general chit chat.",
+    res:
+      "We make [honkers](https://inkthought.codes/honkers) and Supernal. You probably joined because of honkers. Feel free to ask questions or just chill out! We'll post behind-the-scenes in <#804938313215246376> and exclusive announcements in <#693072827481456692>. Enjoy!",
     aliases: ["it", "welcome"],
   },
 };
