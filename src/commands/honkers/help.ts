cfb7538a269313981f105d2508fc6aa542ddff99
geenva <geenva@users.noreply.github.com>
2021-02-22 21:06:47 +0800

    feat: honkhelp

---
+++ src/commands/honkers/help.ts
@@ -0,0 +1,13 @@
+import { Command as CMD } from "../..";
+
+const Command: CMD = {
+  info: {
+    name: "honkhelp",
+    title: "Having issues with honkers?",
+    res:
+      "Please check our [troubleshooting guide](https://docs.google.com/document/d/1DJ3KzXoG8A5Bry2Hi6Lf4Qy52tG3tBhRHQuUJSbS-To/edit)! In case of further issues that you can't fix, please visit <#736835165468491788> and post your question.",
+    aliases: ["hh"],
+  },
+};
+
+export default Command;
